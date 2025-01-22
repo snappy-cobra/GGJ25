74fe656facf56e9b3e4280df6699c9d7c5aebcde
troido <troido@protonmail.com>
2025-01-22 20:32:07 +0100

    joinable players

--- game/code/player.gd
+++ game/code/player.gd
@@ -1,7 +1,14 @@
+class_name Player
 extends CharacterBody2D
 
 const speed = 50
+var id: String
+var display_name: String
+
 
 func _physics_process(_delta: float) -> void:
-	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
+	#velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
 	move_and_slide()
+
+func move(movement: Vector2) -> void:
+	velocity = movement.normalized() * speed
