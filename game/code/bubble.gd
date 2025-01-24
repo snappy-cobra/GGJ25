0b09bf5e99428acffdd8492409929a02f92bde55
troido <troido@protonmail.com>
2025-01-24 21:03:11 +0100

    bubbles can be popped by mouse

--- game/code/bubble.gd
+++ game/code/bubble.gd
@@ -11,3 +11,12 @@ static func create(pos: Vector2i) -> Bubble:
 	bubble.pos = pos
 	bubble.position = pos * size
 	return bubble
+
+func pop() -> void:
+	$Unpopped.hide()
+	$Popped.show()
+
+
+func _on_debug_input_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		pop()
