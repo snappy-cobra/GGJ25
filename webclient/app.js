24152713bca9cfc20ee4e713bb6598833751468d
timostrating <timostrating@home.nl>
2025-01-24 23:19:10 +0100

    put the phoenix js logic in the webclient

--- webclient/app.js
+++ webclient/app.js
@@ -1,3 +1,21 @@
+// token for authentication. Read below how it should be used.
+let socket = new window.Phoenix.Socket("http://localhost:4000/socket", {params: {token: "oke"}})
+
+// Finally, connect to the socket:
+socket.connect()
+
+// Now that you are connected, you can join channels with a topic.
+// Let's assume you have a channel with a topic named `room` and the
+// subtopic is its id - in this case 42:
+let channel = socket.channel("godot", {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 var bubblePopFunction = function() {
     navigator.vibrate(200);
     var sound = new Howl({
@@ -5,6 +23,10 @@ var bubblePopFunction = function() {
     });
       
     sound.play();
+
+    console.log("POP")
+    channel.push("pop", {x: getRandomInt(9), y: getRandomInt(9)});
+
 
     if (typeof navigator.mozVibrate === 'function') {
         navigator.mozVibrate(200);
