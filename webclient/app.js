b2d49afef1c1129e784bf8ad95b8e8be3b224ac3
timostrating <timostrating@home.nl>
2025-01-24 18:41:32 +0100

    Possible firefox support

--- webclient/app.js
+++ webclient/app.js
@@ -2,6 +2,7 @@ canvas = document.getElementById("canvas")
 
 canvas.addEventListener('click', function() { 
     navigator.vibrate(200);
+    navigator.mozVibrate(200);
     console.log("Vibrate");
 }, false);
 
