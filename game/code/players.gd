bdc7bbb3dddff34a8739ea6eaac7578d543d7147
troido <troido@protonmail.com>
2025-01-25 22:35:26 +0100

    player gets assigned random team when joining

--- game/code/players.gd
+++ game/code/players.gd
@@ -10,10 +10,28 @@ var next_team: int = 0
 func player_join(id: String) -> String:
 	if has_node(id):
 		return "Player %s already exists" % id
-	var player_id_num = int(id)
-	var player: Player = Player.create(id, id, teams[player_id_num % teams.size()]) 
+	var player: Player = Player.create(id, id, smallest_team())
 	add_child(player)
 	return ""
+
+func smallest_team() -> Player.Team:
+	# this is way more complicated than it should be, but I don't want 2 persistent states
+	var ppt = {}
+	for team in teams:
+		ppt[team] = 0
+	for player in get_children():
+		ppt[player.team] += 1
+	var least_players = 1_000_000_000
+	var smallest: Player.Team = null
+	var rteams = teams.duplicate()
+	rteams.shuffle()
+	for team in rteams:
+		if ppt[team] < least_players:
+			smallest = team
+			least_players = ppt[team]
+	assert(smallest != null)
+	return smallest
+
 
 func check_full() -> bool:
 	return get_child_count() >= 2
@@ -30,8 +48,7 @@ func has_player(id) -> bool:
 
 func get_player(id) -> Player:
 	return get_node(id)
-	
-	
+
 func assign_teams(preferredTeamsNumber: int = 2) -> void:
 	var i: int = 0
 	for p in get_children():
