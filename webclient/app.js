0057d832fcbc3fe1fe14f169b7cf231381d5b99d
timostrating <timostrating@home.nl>
2025-01-25 08:46:36 +0100

    send back the correct index from the webclient

--- webclient/app.js
+++ webclient/app.js
@@ -1,33 +1,36 @@
 // token for authentication. Read below how it should be used.
 let socket = new window.Phoenix.Socket("http://localhost:4000/socket", {params: {token: "oke"}})
 
-// Finally, connect to the socket:
+// connect to the websocket:
 socket.connect()
-
-// Now that you are connected, you can join channels with a topic.
-// Let's assume you have a channel with a topic named `room` and the
-// subtopic is its id - in this case 42:
 let channel = socket.channel("godot", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+
+var SIZE = 16; 
+
 
 var bubblePopFunction = function() {
-    navigator.vibrate(200);
+    
+    // Sound
     var sound = new Howl({
         src: ['pop.mp3']
     });
       
     sound.play();
 
-    console.log("POP")
-    channel.push("pop", {x: getRandomInt(9), y: getRandomInt(9)});
+    // get the XY of this div
+    var index = this.getAttribute("id");
+    var index_x = index % SIZE;
+    var index_y = Math.floor(index / SIZE);
 
+    console.log("POP " + index + " ("+index_x+","+index_y+")")
+    channel.push("pop", {x: index_x, y: index_y});
 
+    // Vibrate
+    navigator.vibrate(200);
     if (typeof navigator.mozVibrate === 'function') {
         navigator.mozVibrate(200);
     }
@@ -55,7 +58,8 @@ const svgStarString = `
 
 for (var i = 0; i < 100; i++) {
     const cell = document.createElement('div');
-    cell.className = "cell"
+    cell.className = "cell";
+    cell.setAttribute('id', i);
 
     board.appendChild(cell);
 
